f2d7beb23e3d7a5a38c811fdea8c079da46b59a6
Chase Bledsoe <cb986@byu.edu>
2023-12-07 11:11:30 -0700

    Functioning Create Budget

--- src/budget/budget.jsx
+++ src/budget/budget.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import BudgetClass from './budgetClass';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 export function Budget(){
+    const navigate = useNavigate();
+
+    const [budgetCounter, setBudgetCounter] = React.useState(1);
+    const [name, setName] = React.useState('');
+    const [date, setDate] = React.useState('');
+    const [amount, setAmount] = React.useState('');
+    const [notes, setNotes] = React.useState('');
+
+    //Creates a new budget to be filled in by the form
+    async function createBudget(){
+        const newBudget = new BudgetClass(budgetCounter, name, date, amount, notes);
+    
+        console.log(newBudget);
+        
+    
+        const response = await fetch('/api/budget', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(newBudget),
+        });
+    
+        // Store what the service gave us as the high scores
+        const budgets = await response.json();
+        localStorage.setItem("Budget", JSON.stringify(budgets));
+    
+        //Move to console
+        navigate('/console');
+    }
+
+
     return (
         <main className="content pad-text">
             <div className="text-center">
@@ -9,25 +42,45 @@ export function Budget(){
                     <div className="form-group row justify-content-center">
                         <label for="name" className="col-sm-2 col-form-label">Name</label>
                         <div className="col-sm-5">
-                        <input type="text" className="form-control" id="name" placeholder="Budget Name"/>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Budget Name"/>
                         </div>
                     </div>
                     <div className="form-group row justify-content-center">
                         <label for="date" className="col-sm-2 col-form-label">Date Active</label>
                         <div className="col-sm-5">
-                        <input type="date" className="form-control" id="date" placeholder="Password"/>
+                        <input 
+                            type="date" 
+                            className="form-control" 
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            placeholder="Password"/>
                         </div>
                     </div>
                     <div className="form-group row justify-content-center">
                         <label for="amount" className="col-sm-2 col-form-label">Amount In</label>
                         <div className="col-sm-5">
-                        <input type="number" className="form-control" id="amount" placeholder="$"/>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            placeholder="$"/>
                         </div>
                     </div>
                     <div className="form-group row justify-content-center">
                         <label for="notes" className="col-sm-2 col-form-label">Notes</label>
                         <div className="col-sm-5">
-                        <input type="text" className="form-control" id="notes" placeholder="Notes"/>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            value={notes}
+                            onChange={(e) => setNotes(e.target.value)} 
+                            placeholder="Notes"/>
                         </div>
                     </div>
                     
@@ -36,7 +89,7 @@ export function Budget(){
 
                     <div className="form-group row justify-content-center">
                         <div className="col-sm-5">
-                        <button type="button" className="btn btn-primary" onclick="createBudget()">Submit</button>
+                        <Button type="button" className="btn btn-primary" onClick={() => createBudget()}>Submit</Button>
                         </div>
                     </div>
                     </form>
---
+++ src/budget/budgetClass.js
@@ -0,0 +1,12 @@
+export default class BudgetClass {
+    constructor(Number, Name, Date, Amount, Notes){
+        this.Number = Number;
+        this.Name = Name;
+        this.Date = Date;
+        this.Amount = Amount;
+        this.Notes = Notes;
+        this.Expenses = [];
+      }
+  }
+
+  
